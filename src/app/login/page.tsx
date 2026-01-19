e4ee4bbb00389ad3c82001e41199cecef4740900
Raveesha Nethsarani <raveeshanethsarani963@gmail.com>
2026-01-19 15:23:15 +0530

    chore: extract login background and button animations into separate CSS files; clean up LoginForm component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,10 @@ import LoginForm from '../../components/forms/login-form';
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="relative min-h-screen w-full bg-[#F8F9FA] overflow-hidden">
+    
         <LoginForm />
-      </div>
+      
     </div>
   )
 }
